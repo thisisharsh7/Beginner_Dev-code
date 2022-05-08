259060b654a2cc4aab6b08dfbcd8c736e24c1465
Harsh Kumar <95894045+thisisharsh7@users.noreply.github.com>
2022-05-08 22:41:24 +0530

    javasript files

---
+++ javascript/index4.js
@@ -0,0 +1,22 @@
+console.log("harsh");
+let myVar;
+myVar=34;
+console.log(myVar, (typeof myVar));
+myVar= String(34);
+console.log(myVar, typeof myVar);
+let booleanVar= true;
+console.log(booleanVar, typeof booleanVar);
+booleanVar=String(true);
+console.log(booleanVar, typeof booleanVar);
+let arr=[1,3,4,5];
+console.log(arr.length, typeof arr);
+arr=String([1,2,3,5]);
+console.log(arr.length, typeof arr);
+let i=new Date();
+console.log(i.toString());
+let game='134';
+console.log(game);
+console.log(Number(game));
+let x='34.23';
+console.log(parseInt(x));
+
